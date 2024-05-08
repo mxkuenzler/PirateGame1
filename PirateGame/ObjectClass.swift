075ca4c4b8afd6eff6bea8db5f5aa3258155ad66
Liav Ost <liavost2005@gmail.com>
2024-05-08 10:10:11 -0400

    Version 0.02

--- PirateGame/ObjectClass.swift
+++ PirateGame/ObjectClass.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import RealityKit
+import RealityKitContent
+import _RealityKit_SwiftUI
+
+class Object {
+    
+    private var Model:ModelEntity?
+    private var ID:Int?
+    
+    init(Model: ModelEntity, ID: Int) {
+        self.Model = Model
+        self.ID = ID
+    }
+    
+    func spawnObject(rContent: RealityViewContent?, lighting:EnvironmentResource? ,position:SIMD3<Float>) {
+        
+//        initiatePhysicsBody(rContent)
+//        initiateLighting(lighting)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
