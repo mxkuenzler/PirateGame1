75ba778bf4ddcfc43daa41ed6a3b588af77bd66e
Max Kuenzler <mxkuenzler@gmail.com>
2024-05-08 11:59:39 -0400

    Added Block and Floor

---
+++ PirateGame/Block.swift
@@ -0,0 +1,26 @@
+//
+//  Block.swift
+//  PirateGame
+//
+//  Created by Maxwell Kuenzler on 5/8/24.
+//
+
+import Foundation
+import RealityKit
+import RealityKitContent
+import _RealityKit_SwiftUI
+
+class Block: Object {
+    
+    var HP: Int
+    
+    init(Material: [any Material], Health: Int){
+        
+        HP = Health
+        
+        let BlockModel = ModelEntity(mesh: .generateBox(size: 0.25), materials: Material)
+        
+        super.init(Model: BlockModel, ID: 0)
+    }
+    
+}
---
+++ PirateGame/Floor.swift
@@ -0,0 +1,25 @@
+//
+//  Floor.swift
+//  PirateGame
+//
+//  Created by Maxwell Kuenzler on 5/8/24.
+//
+
+import Foundation
+import RealityKit
+import RealityKitContent
+import _RealityKit_SwiftUI
+
+class Floor: Object{
+    
+    init(){
+        
+        let FloorModel = ModelEntity(mesh: .generatePlane(width: 20, depth: 20), materials: [OcclusionMaterial()])
+        
+        let FloorId = 0
+        
+        super.init(Model: FloorModel, ID: FloorId)
+        
+        self.getModel()?.setPosition(SIMD3<Float>(x: 0, y: -0.001, z: 0), relativeTo: self.getModel())
+    }
+}
