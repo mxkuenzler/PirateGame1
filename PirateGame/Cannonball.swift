26c3193f0168c433ae5ea245f71d901fe9347aca
Max Kuenzler <mxkuenzler@gmail.com>
2024-05-08 11:59:39 -0400

    Added Block, Floor, and Cannonball

---
+++ PirateGame/Cannonball.swift
@@ -0,0 +1,22 @@
+//
+//  Cannonball.swift
+//  PirateGame
+//
+//  Created by Maxwell Kuenzler on 5/8/24.
+//
+
+import Foundation
+import RealityKit
+import RealityKitContent
+import _RealityKit_SwiftUI
+
+class Cannonball: Object {
+    
+    init(){
+        
+        let CannonballModel = ModelEntity(mesh: .generateSphere(radius: 0.25), materials: [SimpleMaterial(color: .black, roughness: 0.8, isMetallic: false)])
+        
+        super.init(Model: CannonballModel, ID: 0)
+        
+    }
+}
